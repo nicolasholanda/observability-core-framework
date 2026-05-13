aa962e408a75b3afb5ec3f86b45d2ddd2ea202f4
nicolasholanda <nikolas.nhm14@gmail.com>
2026-05-12 21:41:57 -0300

    Initial commit

---
+++ src/main/java/com/observability/core/ObservabilityCoreApplication.java
@@ -0,0 +1,12 @@
+package com.observability.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObservabilityCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObservabilityCoreApplication.class, args);
+    }
+}
